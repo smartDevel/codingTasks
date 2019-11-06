b07f3159b3004b7a4c4c0c8d0b17895a1bb67727
smartDevel <mailbochs@gmail.com>
2019-11-06 15:07:56 +0100

    numImagFriends added

---
+++ codeChallengesJSInterm/imagFriends/imaginaryFriends.js
@@ -0,0 +1,9 @@
+
+const numImaginaryFriends = numFriends => {
+    return Math.round(numFriends * 0.33);
+  }
+  
+
+   console.log(numImaginaryFriends(18)); // Should print 6
+   console.log(numImaginaryFriends(35));
+  
